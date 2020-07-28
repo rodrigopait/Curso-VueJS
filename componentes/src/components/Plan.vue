f2b43c3ab29e7add556c49656fa28862e6d2d296
Rodrigo Pait <rodripait@gmail.com>
2020-07-27 21:51:50 -0300

    chapter 92

---
+++ componentes/src/components/Plan.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="plan">
+    <div class="descripcion">
+      <span class="titulo">{{nombre}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Plan",
+  props: {
+    nombre: {
+      type: String,
+      default: "Plan 1 - Principiante",
+      required: true,
+    },
+    precio: Number,
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+</style>
