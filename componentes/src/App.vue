b23b5373c377e895ccf8c7ec7bab2ffa92a9f586
Rodrigo Pait <rodripait@gmail.com>
2020-07-27 21:36:19 -0300

    chapter 91

---
+++ componentes/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png"><br/>
+		<contador/>
+		<contador/>
+		<contador/>
+		<contador/>
+		<contador/>
+  </div>
+</template>
+
+<script>
+import Contador from './components/Contador.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Contador
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ componentes/src/components/Contador.vue
@@ -0,0 +1,23 @@
+<template>
+	<button @click="contar">{{contador }}</button>
+</template>
+
+<script>
+export default {
+	name: 'Contador',
+	data(){
+		return {
+			contador : 0
+		}
+	},
+	methods:{
+		contar: function(){
+			this.contador++
+		}
+	}
+}
+</script>
+
+<style>
+
+</style>
