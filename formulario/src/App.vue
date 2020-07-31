aba9a2befd1ca83b905e1b7483482290c0ded021
Rodrigo Pait <rodripait@gmail.com>
2020-07-31 17:09:27 -0300

    section-12

---
+++ formulario/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <formulario></formulario>
+  </div>
+</template>
+
+<script>
+
+import Formulario from './components/Formulario.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Formulario
+  }
+}
+</script>
+
+<style>
+
+</style>
